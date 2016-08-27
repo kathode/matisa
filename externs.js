cdef7342f086d6f070f80b245fe3618a23256636
Pedro Ribeiro <pedro.ribeiro@york.ac.uk>
2016-08-28 00:16:47 +0100

    Added externs.js as required for closure not to rename stuff.

---
+++ externs.js
@@ -0,0 +1,12 @@
+  // Externs for closure:
+  function extension(){};
+  function getURL(){};
+
+  function Hub(){};
+  function Queue(){};
+  function Register(){};
+  function StartupHook(){};
+
+  var MathJax;
+  var isabelle;
+  function parse(){};
